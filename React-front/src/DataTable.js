10251d7e2780723e8aeb6aa575b985add70fc660
Fitru <fitru@ZipcodersMBP100.fios-router.home>
2022-08-21 22:27:05 -0400

    Adding a datatable

---
+++ React-front/src/DataTable.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid'
+
+const columns = [
+    {field: 'route', headerName: 'Route', width: 100},
+    {field: 'route', headerName: 'Route', width: 100},
+    
+
+
+]
